5f55e20b31c58749de8ee05b8cfc745cd16a6ee1
rak55 <59727832+rak55@users.noreply.github.com>
2024-03-23 12:11:56 -0500

    Update mm_utils.py

--- llava/mm_utils.py
+++ llava/mm_utils.py
@@ -177,10 +177,10 @@ def process_images(images, image_processor, model_cfg):
             new_images.append(image)
     else:
         return image_processor(images, return_tensors='pt')['pixel_values']
-    print(new_images[0].shape,new_images[1].shape)
+    #print(new_images[0].shape,new_images[1].shape)
     if all(x.shape == new_images[0].shape for x in new_images):
         new_images = torch.stack(new_images, dim=0)
-        print(new_images.shape)
+        #print(new_images.shape)
     return new_images
 
 
