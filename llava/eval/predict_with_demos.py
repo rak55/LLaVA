70b3a1c60497c3d2fb679f02476e77b77e12e375
rak55 <59727832+rak55@users.noreply.github.com>
2024-03-29 02:33:08 -0500

    Update predict_with_demos.py

--- llava/eval/predict_with_demos.py
+++ llava/eval/predict_with_demos.py
@@ -59,7 +59,7 @@ def eval_model(args):
 
     dataset = list(read_jsonl(args.dataset))
     demos = list(read_jsonl(args.demos))
-    demos = demos[1:args.num_demos]
+    demos = demos[1:args.num_demos+1]
     ex_demos = []
     for d_item in demos:
         ex_demos.append(
