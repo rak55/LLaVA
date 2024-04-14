ba2f5bd3df11cbd02660615b5a5c35f3749363cd
rak55 <59727832+rak55@users.noreply.github.com>
2024-04-14 21:08:00 +0530

    Update predict_with_demosv1_5.py

--- llava/eval/predict_with_demosv1_5.py
+++ llava/eval/predict_with_demosv1_5.py
@@ -96,8 +96,6 @@ def load_pretrained_model(
         kwargs["load_in_4bit"] = True
         kwargs["quantization_config"] = BitsAndBytesConfig(
             load_in_4bit=True,
-              kwargs["quantization_config"] = BitsAndBytesConfig(
-            load_in_4bit=True,
             bnb_4bit_compute_dtype=torch.bfloat16 if load_bf16 else torch.float16,
             bnb_4bit_use_double_quant=True,
             bnb_4bit_quant_type="nf4",
